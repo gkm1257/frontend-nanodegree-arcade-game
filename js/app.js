5ea0d7175ba9eadbb9ff91f933138eddca32158b
gkm1257 <gkm1257@gmail.com>
2018-01-03 23:21:27 +0800

    feat: Add Enemy and .update function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = -100;
+    this.y = y || 60;
+    this.speed = speed || 50;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +17,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + dt * this.speed;
+    if (this.x > 505) { this.x = -100; }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -29,6 +34,9 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const bug1 = new Enemy();
+const bug2 = new Enemy(143, 100);
+let allEnemies = [ bug1, bug2 ];
 
 
 
