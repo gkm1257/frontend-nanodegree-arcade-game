7cc75385ed788393cb74182572b5d2e401a4fdc7
gkm1257 <gkm1257@gmail.com>
2018-01-04 19:32:20 +0800

    feat: Randomly generate enemies

--- js/app.js
+++ js/app.js
@@ -1,41 +1,34 @@
 // Enemies our player must avoid
-var Enemy = function(y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = -100;
+var Enemy = function(x, y, speed, startX) {
+    this.x = x || -100; // Initial position
     this.y = y || 60;
-    this.speed = speed || 50;
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.speed = speed || 50; // Enemy moving speed
+    // Use random starting x position to decrease predictability
+    this.startX = startX || -100;
+    this.sprite = 'images/enemy-bug.png'; // Enemy image
 };
 
-// Update the enemy's position, required method for game
+// Update the enemy's position
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x = this.x + dt * this.speed;
-    if (this.x > 505) { this.x = -100; }
+    if (this.x > 505) { this.x = this.startX; }
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
 class Player {
     constructor(person = 'images/char-boy.png') {
-        this.x = 202;
+        this.x = 202; // Initial position
         this.y = 380;
-        this.person = person;
+        this.person = person; // Character's image file name
     }
 
+    // Draw the player on the screen
     render() {
         ctx.drawImage(Resources.get(this.person), this.x, this.y);
     }
@@ -44,6 +37,7 @@ class Player {
 
     }
 
+    // Change positions according to keyboard input
     handleInput(direction) {
         if (direction === 'left' && this.x > 0) {
             this.x = this.x - 101;
@@ -58,12 +52,15 @@ class Player {
 };
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-const bug1 = new Enemy();
-const bug2 = new Enemy(143, 100);
-let allEnemies = [ bug1, bug2 ];
+// Generate enemies with random number, speed and starting x-coordinate
+let allEnemies = [];
+let enemyNum = Math.floor(Math.random() * 3) + 5;
+for (let i = 0; i < enemyNum; i++) {
+    let x = -Math.floor(Math.random() * 1000) - 100,
+        y = (i % 3) * 83 + 60,
+        speed = Math.floor(Math.random() * 200) + 250;
+    allEnemies.push(new Enemy(x, y, speed, x));
+}
 
 const player = new Player();
 
